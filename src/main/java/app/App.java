032d7e0fb60e5d91f631a6e0edb846e2bfb118b5
Alexander Terekhov <aterekhov.consultant@videologygroup.com>
2018-05-30 11:58:56 +0300

    First commit

---
+++ src/main/java/app/App.java
@@ -0,0 +1,18 @@
+package app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class App {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String hello() {
+        return "HELLO!";
+    }
+}
+
+
+
---
+++ src/main/java/app/Application.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
